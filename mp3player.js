e0c253d9ebccfc8d9a44e0c6e6b3f06efd8b66fe
romain <romain.bourget@itkweb.com>
2015-09-30 08:44:55 +0000

    fix mp3player oubli de commit

---
+++ mp3player.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var exec = require('exec');
+
+var COMMAND = "mpg321 -g 100 ";
+
+var play = function play(filePath){
+        console.log("WHATTA MAAAAAAASK !!!!");
+	exec(COMMAND + filePath, function(err, out, code){
+	    if (err instanceof Error)
+    			throw err;
+  	    process.stderr.write(err);
+      	    process.stdout.write(out);
+      	    process.exit(code);
+	});
+};
+
+module.exports = {
+	play: play
+};
